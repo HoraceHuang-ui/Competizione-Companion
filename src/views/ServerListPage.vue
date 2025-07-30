ab94ca2a90cebff896a7c86523ce58ea568bb0a3
HoraceHuang-ui <horacehuang17@gmail.com>
2025-07-30 09:48:51 +0800

    feat: rest pages placeholder

---
+++ src/views/ServerListPage.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div
+    class="size-full flex flex-col justify-center items-center pb-8 relative"
+  >
+    <div>在做了在做了（新建文件夹</div>
+    <ul class="mt-4">
+      <li>显示服务器列表及查询、筛选</li>
+      <li>可唤起acc-connector并自动配置hook</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
